61d4dc775ffcc52ff4acf5393ef744af37280fec
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2021-12-28 02:14:38 +0330

    final2

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import { useLanguage } from './context';
-import { useContext } from 'react';
-import { createContext } from 'react';
+// import { useContext } from 'react';
+// import { createContext } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap';
